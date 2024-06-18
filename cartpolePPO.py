8cb33e23fba940109bbeb21a3c9c26d7eaf1141f
nklloyd <nicholask.lloyd@gmail.com>
2024-06-19 01:51:22 +0200

    Updates

--- cartpolePPO.py
+++ cartpolePPO.py
@@ -1,14 +1,24 @@
 import gymnasium as gym
 import numpy as np
 from stable_baselines3 import PPO
+from stable_baselines3.common.callbacks import EvalCallback, StopTrainingOnRewardThreshold
+from stable_baselines3.common.monitor import Monitor
+import os
 
-env = gym.make("CartPole-v1", render_mode="human")
+log_dir = "gymgraphs/"
+os.makedirs(log_dir, exist_ok=True)
+env = gym.make("CartPole-v1")
+env = Monitor(env, log_dir)
 
-model = PPO("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=10000, log_interval=4)
-model.save("ppo_cartpole")
+model = PPO("MlpPolicy", env, verbose=1, tensorboard_log="./ppo_cartpole_graph/")
 
-model = PPO.load("ppo_cartpole")
+eval_callback = EvalCallback(env, best_model_save_path='./logs/',
+                             log_path='./logs/', eval_freq=500,
+                             deterministic=True, render=False)
+
+model.learn(total_timesteps=10000, log_interval=4, callback=eval_callback)
+model.save("ppo_cartpole2")
+model = PPO.load("ppo_cartpole2", env=env)
 
 obs, info = env.reset()
 while True:
@@ -17,3 +27,5 @@ while True:
     env.render()
     if terminated or truncated:
         obs, info = env.reset()
+
+#tensorboard --logdir=./ppo_cartpole_graph/
--- pendulumTD3.py
+++ pendulumTD3.py
@@ -1,14 +1,24 @@
 import gymnasium as gym
 import numpy as np
 from stable_baselines3 import TD3
+from stable_baselines3.common.callbacks import EvalCallback, StopTrainingOnRewardThreshold
+from stable_baselines3.common.monitor import Monitor
+import os
 
+log_dir = "gymgraphs/"
+os.makedirs(log_dir, exist_ok=True)
 env = gym.make("Pendulum-v1", render_mode="human")
+env = Monitor(env, log_dir)
 
-model = TD3("MlpPolicy", env, verbose=1)
+model = TD3("MlpPolicy", env, verbose=1, tensorboard_log="./td3_pendulum_graph/")
+
+eval_callback = EvalCallback(env, best_model_save_path='./logs/',
+                             log_path='./logs/', eval_freq=500,
+                             deterministic=True, render=False)
+
 model.learn(total_timesteps=10000, log_interval=4)
-model.save("td3_pendulum")
-
-model = TD3.load("td3_pendulum")
+model.save("td3_pendulum2")
+model = TD3.load("td3_pendulum2", env=env)
 
 obs, info = env.reset()
 while True:
@@ -17,3 +27,5 @@ while True:
     env.render()
     if terminated or truncated:
         obs, info = env.reset()
+
+#tensorboard --logdir=./td3_pendulum_graph/
